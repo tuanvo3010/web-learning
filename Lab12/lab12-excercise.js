51e5f5dfbe69f7d16e270ce8a98f0ee0a28612d1
Tuan Vo <tuanvo3010works@gmail.com>
2022-08-15 22:51:23 +0700

    Add lab12 content

---
+++ Lab12/lab12-excercise.js
@@ -0,0 +1,58 @@
+/**
+ * Kiểm tra số một số nguyên dương bất kỳ có phải số nguyên tố hay không
+ *
+ * Số nguyên tố là số chỉ chia hết cho `1` và chính nó
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {boolean} Số `n` cho có phải số nguyên tố hay không
+ */
+function isPrime(n) {}
+
+/**
+ * Tính tổng các số nguyên tố trong khoảng từ 1 đến n
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {number} Tổng các số nguyên tố trong khoảng từ `1` đến `n`
+ */
+function sumOfPrimes(n) {}
+
+/**
+ * Tìm số Fibonacci thứ n
+ *
+ * Dãy Fibonacci là dãy số bắt đầu bằng 0 và 1, các số tiếp theo bằng tổng 2 số trước nó
+ *
+ * VD: 0 1 1 2 3 5 8 13 ... (Số thứ 1 là 0, số thứ 2 là 1, số thứ 3 là 2, ...)
+ *
+ * VD: `n = 5`, kết quả số Fibonacci thứ 5 là `3`
+ *
+ * @param {number} n Số nguyên dương bất kỳ (n > 0)
+ *
+ * @return {number} Số Fibonacci thứ `n`
+ */
+function fibonacci(n) {}
+
+/**
+ * Tính giai thừa của một số nguyên bất kỳ
+ *
+ * Giai thừa của một số `n = 1 * 2 * 3 * ... * (n - 1) * n`
+ *
+ * @param {number} n Số nguyên dương bất kỳ (n >= 0)
+ *
+ * @return {number} Giai thừa của `n`
+ */
+function factorial(n) {}
+
+/**
+ * Kiểm tra một số nguyên dương bất kỳ có phải số Strong hay không
+ *
+ * Số Strong là số có tổng giai thừa các chữ số bằng chính nó
+ *
+ * VD: 145 = 1! + 4! + 5! = 1 + 24 + 120 = 145
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {boolean} Số `n` có phải là số Strong hay không
+ */
+function isStrong(n) {}
